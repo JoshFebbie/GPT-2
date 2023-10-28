83b244dcbd6e3986d2760b7cb6ead6b853935047
Josh Febbie <joshdfebbie@gmail.com>
2023-10-28 09:00:50 -0700

    Fin

--- src/App.js
+++ src/App.js
@@ -8,38 +8,87 @@ import rocket from './assets/rocket.svg';
 import sendBtn from './assets/send.svg';
 import userIcon from './assets/user_2.jpg';
 import gptImgLogo from './assets/gpticon2.png';
+import { sendMsgToOpenAI } from './openai';
+import { useState, useRef, useEffect } from 'react';
+
 
 function App() {
+  const msgEnd = useRef(null);
+
+  const [input, setInput] = useState('')
+  const [messages, setMessages] = useState([
+    {
+    text: "Hello, I'm FajGPT! I am a virtual assistant powered by GPT-4, one of the most advanced models in OpenAI's Generalized Autoregressive Pre-trained Transformer (GPT) series.  My primary purpose is to assist users by providing information, answering questions, helping with tasks like text generation, tutoring in various subjects, brainstorming, and more. I aim to be a versatile tool for a wide array of text-based tasks. ",
+    isBot: true,
+  }
+])
+
+useEffect(() => {
+  msgEnd.current.scrollIntoView();
+}, [messages])
+
+  const handleSend = async () => {
+      const text = input;
+      setInput('');
+      setMessages([
+        ...messages,
+         { text, isBot: false }
+      ]);
+      const res = await sendMsgToOpenAI(text);
+      setMessages([
+        ...messages,
+        { text, isBot: false },
+        { text: res, isBot: true },
+      ]);
+    }
+
+  const handleEnter = async (e) => {
+    if (e.key === 'Enter') await handleSend();
+  }
+   
+  const handleQuery = async (e) => {
+    const text = e.target.value;
+    setMessages([
+      ...messages,
+       { text, isBot: false }
+    ]);
+    const res = await sendMsgToOpenAI(text);
+    setMessages([
+      ...messages,
+      { text, isBot: false },
+      { text: res, isBot: true },
+    ]);
+  } 
+
   return (
     <div className="App">
       <div className='sidebar'>
         <div className="upperSide">
           <div className="upperSideTop"><img src={gptLogo} alt="Logo" className="logo" /><span className="brand">FajGPT</span></div>
-          <button className="midBtn"><img src={addBtn} alt="new chat" className="addBtn" />New Chat</button>
+          <button className="midBtn" onClick={()=>{window.location.reload()}}><img src={addBtn} alt="new chat" className="addBtn" />New Chat</button>
           <div className="upperSideBottom">
-            <button className="query"><img src={msgIcon} alt="Query" />What is programming?</button>
-            <button className="query"><img src={msgIcon} alt="Query" />How to use an API?</button>
+            <button className="query" onClick={handleQuery} value={"What is Programming?"}><img src={msgIcon} alt="Query" />What is programming?</button>
+            <button className="query" onClick={handleQuery} value={"How to use an API?"}><img src={msgIcon} alt="Query" />How to use an API?</button>
           </div>
         </div>
         <div className="lowerSide">
           <div className="listItems"><img src={home} alt="" className="listItems" />Home</div>
           <div className="listItems"><img src={saved} alt="" className="listItems" />Saved</div>
           <div className="listItems"><img src={rocket} alt="" className="listItems" />Upgrade to PRO</div>
-
         </div>
       </div>
       <div className='main'>
         <div className='chats'>
-          <div className="chat">
-            <img className='chatImg' src={userIcon} alt="" /><p className="txt">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet accusamus omnis aliquid minus numquam iure pariatur repudiandae iste facere autem?</p>
-          </div>
-          <div className="chat bot">
-            <img className='chatImg' src={gptImgLogo} alt="" /><p className="txt">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet accusamus omnis aliquid minus numquam iure pariatur repudiandae iste facere autem?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet accusamus omnis aliquid minus numquam iure pariatur repudiandae iste facere autem?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet accusamus omnis aliquid minus numquam iure pariatur repudiandae iste facere autem?</p>
-          </div>
+          {messages.map((message, i) => 
+            <div key={i} className={message.isBot ? "chat bot" : "chat"}>
+              <img className='chatImg' src={message.isBot ? gptImgLogo : userIcon} alt="ChatGPT" /><p className="txt">{message.text}</p>
+            </div>
+          )}
+          <div ref={msgEnd}/>
         </div>
         <div className="chatFooter">
           <div className='inp'>
-            <input type="text" placeholder='Send a message'/> <button className="send"><img src={sendBtn} alt="Send" /></button>
+            <input type="text" placeholder='Send a message' value={input} onKeyDown={handleEnter} onChange={(e) =>{setInput(e.target.value)}}/><button className="send" onClick={handleSend}><img src={sendBtn} alt="Send" /></button>
           </div>
             <p>ChatGPT may produce inaccurate information about people, places, or facts. ChatGPT August 20 Version.</p>
         </div>
---
+++ src/openai.js
@@ -0,0 +1,95 @@
+// const { Configuration, OpenAiApi } = require('openai');
+// const configuration = new Configuration({ apiKey: "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" });
+// const openai = new OpenAiApi(configuration);
+
+// export async function sendMsgToOpenAI(message) {
+//     const res = await openai.createCompletion ({
+//         model: "text-davinci-003",
+//         prompt: message,
+//         temperature: 0.7,
+//         max_tokens: 256,
+//         top_p: 1,
+//         frequency_penalty: 0,
+//         presense_penalty: 0
+//     })
+//     return res.data.choices[0].text
+// }
+
+
+// const openai = require('openai');
+// const Configuration = openai.Configuration;
+// const OpenAiApi = openai.OpenAiApi;
+
+// const configuration = new Configuration({ apiKey: "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" }); // Make sure to replace this with your API key if it's different
+// const api = new OpenAiApi(configuration);
+
+// export async function sendMsgToOpenAI(message) {
+//     const res = await api.createCompletion({
+//         model: "text-davinci-003",
+//         prompt: message,
+//         temperature: 0.7,
+//         max_tokens: 256,
+//         top_p: 1,
+//         frequency_penalty: 0,
+//         presense_penalty: 0
+//     });
+//     return res.data.choices[0].text;
+// }
+
+
+
+
+
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+    apiKey: "",
+    dangerouslyAllowBrowser: true
+});
+
+
+export async function sendMsgToOpenAI(message) {
+    const res = await openai.completions.create({
+      model: "gpt-3.5-turbo-instruct",
+      prompt: message,
+      max_tokens: 256,
+      temperature: 0,
+    });
+  
+    console.log("API Response:", res);  // Log the full response
+  
+    if (res && res.choices) {
+        return res.choices[0].text;
+      } else {
+        throw new Error("Invalid API response");
+      }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export async function sendMsgToOpenAI(message) {
+//   const res = await openai.completions.create({
+//     model: "gpt-3.5-turbo-instruct",
+//     prompt: message,
+//     max_tokens: 256,
+//     temperature: 0,
+//   });
+
+//     return res.data.choices[0].text;
+
+// }
+
+
